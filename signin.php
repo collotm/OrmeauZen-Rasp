f9add32ba60358f1fee74d931c4fc156836d35c9
collotm <32330365+collotm@users.noreply.github.com>
2018-03-21 11:30:21 +0100

    signup and signin

--- signin.php
+++ signin.php
@@ -20,17 +20,12 @@
   <body class="text-center">
     <form class="form-signin" action="includes/signup.inc.php" method="POST">
       <img class="mb-4" src="images/frah.jpg" alt="">
-      <h1 class="h3 mb-3 font-weight-normal">Connectez-vous à OrmeauZen V1.0</h1>
+      <h1 class="h3 mb-3 font-weight-normal">Connectez-vous à OrmeauZen v2.0</h1>
       <label for="inputEmail" class="sr-only">Mail Adress</label>
-      <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Adresse mail" required autofocus>
+      <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Adresse E-mail/Identifiant" required autofocus>
       <label for="inputPassword" class="sr-only">Password</label>
       <input type="password" id="inputPassword" name="pwd" class="form-control" placeholder="Mot de passe" required>
-      <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me"> Se souvenir de moi
-        </label>
-      </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Accédez à votre compte !</button>
+      <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Accédez à votre compte</button>
       <p class="mt-5 mb-3 text-muted">&copy; OrmeauZen 2017-2018</p>
     </form>
   </body>
--- signup.php
+++ signup.php
@@ -20,10 +20,11 @@
   <body class="text-center">
     <form class="form-signin" action="includes/signup.inc.php" method="POST">
       <img class="mb-4" src="images/frah.jpg" alt="">
-      <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Adresse mail" required autofocus>
-      <input type="password" id="inputPassword" name="pwd" class="form-control" placeholder="Mot de passe" required>
-      <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Adresse mail" required autofocus>
-      <input type="password" id="inputPassword" name="pwd" class="form-control" placeholder="Mot de passe" required>
+      <h1 class="h3 mb-3 font-weight-normal">Inscription à OrmeauZen v2.0</h1>
+      <input type="text" id="inputEmail" name="first" class="form-control" placeholder="Nom" required autofocus>
+      <input type="text" id="inputPassword" name="last" class="form-control" placeholder="Prénom" required>
+      <input type="text" id="inputEmail" name="email" class="form-control" placeholder="Adresse E-mail" required autofocus>
+      <input type="text" id="inputPassword" name="uid" class="form-control" placeholder="Identifiant" required>
       <input type="password" id="inputPassword" name="pwd" class="form-control" placeholder="Mot de passe" required>
       <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Inscription</button>
       <p class="mt-5 mb-3 text-muted">&copy; OrmeauZen 2017-2018</p>
